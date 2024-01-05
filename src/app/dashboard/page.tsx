79674f69ad67c70ea4bbdc079e6062f355c56f98
bmalkes <bmalkes@gmail.com>
2024-01-05 18:37:30 +0200

    feat:Add home and header

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Container from "@/components/container";
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <Container>
+      <h1>Dashboard</h1>
+    </Container>
+  );
+};
+
+export default Dashboard;
